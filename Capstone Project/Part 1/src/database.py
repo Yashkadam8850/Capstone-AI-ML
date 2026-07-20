c695a832490f558ec9631cab32d0f371680a771b
Yash Santosh kadam <yashkadam8850@gmail.com>
2026-07-20 21:29:00 +0530

    Fix file path for dataset in database.py

--- Capstone Project/Part 1/src/database.py
+++ Capstone Project/Part 1/src/database.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 
-data = pd.read_csv("c:/Users/Yash/OneDrive/Desktop/CAPSTONE PROJECT/Data/Part 1/clean_dataset/googleplaystore_cleaned.csv")
+data = pd.read_csv("CAPSTONE PROJECT/Data/Part 1/clean_dataset/googleplaystore_cleaned.csv")
 
 Connection=sqlite3.connect("Google_Playstore_Data.db")
 data.to_sql(
