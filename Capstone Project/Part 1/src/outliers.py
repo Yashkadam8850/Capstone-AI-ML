87360416700673e739ea8542b52479abbce8a7d1
Yash Santosh kadam <yashkadam8850@gmail.com>
2026-07-20 21:29:38 +0530

    Refactor outlier detection and visualization code

--- Capstone Project/Part 1/src/outliers.py
+++ Capstone Project/Part 1/src/outliers.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("c:/Users/Yash/OneDrive/Desktop/CAPSTONE PROJECT/Data/Part 1/clean_dataset/googleplaystore_cleaned.csv")
+data = pd.read_csv("CAPSTONE PROJECT/Data/Part 1/clean_dataset/googleplaystore_cleaned.csv")
 
 # Numeric columns to check
 columns = ["Rating", "Reviews", "Price",]
